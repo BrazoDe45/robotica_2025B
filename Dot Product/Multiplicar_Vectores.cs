ddd2d913a9b6a6ad475164195f525cd7629638bf
BrazoDe45 <Luis_Cardenas870@outlook.com>
2025-08-24 06:08:45 -0600

    Dot_product y Metodo de Entrad de datos

---
+++ Dot Product/Multiplicar_Vectores.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot_Product
+{
+    public class Dot_Product
+    {
+        static public double multiplicacion(double[] v1, double[] v2)
+        {
+            if (v1.Length != v2.Length)
+                throw new Exception("Las dimensiones de los vectores no coinciden.");
+
+            double resultado = 0;
+
+            int size;
+
+            size = v1.Length;
+
+            for(int i=0; i < size; i++)
+                resultado += v1[i] * v2[i];
+
+            return resultado;
+        }
+    }
+}
--- ejercicio_1/Program.cs
+++ ejercicio_1/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using AlgebraLineal;
+using Dot_Product;
 
 namespace ejercicio_1
 {
@@ -11,29 +13,60 @@ namespace ejercicio_1
     {
         static void Main(string[] args)
         {
-            //TAREA: Ciclarlo hasta que uno de los vectores ingresados sea "Q"
-            Console.WriteLine("Ingresa los elementos del primer vector separados por una coma. ");
-
-            //RECOGER LINEA DE TEXTO Y SEPARAR NUMEROS
-            double[] a = Array.ConvertAll<string, double>(Console.ReadLine().Split(','),double.Parse);
-
-            Console.WriteLine("Ingresa los elementos del segundo vector separados por una coma. ");
-            //RECOGER LINEA DE TEXTO Y SEPARAR NUMEROS
-            double[] b = Array.ConvertAll<string, double>(Console.ReadLine().Split(','), double.Parse);
-
-            //REALIZAR SUMA DE VECTORES
-            double[] c;
-            try
+            while (true)
             {
-                c = Vectores.Suma(a, b);
+                //TAREA: Ciclarlo hasta que uno de los vectores ingresados sea "Q"
+                Console.WriteLine("Puedes ingresar Q en cualquiera de los vectores para terminar el programa.\n");
+                Console.WriteLine("Ingresa los elementos del primer vector separados por una coma. ");
+
+                string[] a_str = Console.ReadLine().Split(',');
+                if (a_str[0] == "Q")
+                    break;
+
+                //RECOGER LINEA DE TEXTO Y SEPARAR NUMEROS
+                double[] a = Array.ConvertAll<string, double>(a_str, double.Parse);
+
+                Console.WriteLine("Ingresa los elementos del segundo vector separados por una coma. ");
+                //RECOGER LINEA DE TEXTO Y SEPARAR NUMEROS
+                string[] b_str = Console.ReadLine().Split(',');
+                if (b_str[0] == "Q")
+                    break;
+
+                double[] b = Array.ConvertAll<string, double>(b_str, double.Parse);
+
+                /*
+                //REALIZAR SUMA DE VECTORES
+                double[] c;
+                try
+                {
+                    c = Vectores.Suma(a, b);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Se ha detectado la excepción {ex.Message}");
+                    return;
+                }
+                Console.WriteLine("El resultado de la suma es ");
+                Console.WriteLine(String.Join(", ", c));
             }
-            catch (Exception ex) 
-            {
-                Console.WriteLine($"Se ha detectado la excepción {ex.Message}");
-                return;
+                */
+
+                //REALIZAR MULTIPLICACION DE VECTORES
+                double resultado_multiplicacion;
+                try
+                {
+                    resultado_multiplicacion = Dot_Product.Dot_Product.multiplicacion(a, b);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Se ha detectado la excepción {ex.Message}");
+                    return;
+                }
+                Console.WriteLine($"El resultado de la suma es {resultado_multiplicacion}");
+
+                Console.WriteLine("Presiona cualquier tecla para continuar...");
+                Console.ReadKey();
             }
-            Console.WriteLine("El resultado de la suma es ");
-            Console.WriteLine(String.Join(", ", c));
         }
     }
 }
